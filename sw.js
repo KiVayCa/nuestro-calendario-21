72092afed2184eb7d152f749c077d6fba3a38232
KiVayCa <nickolasmorales82@gmail.com>
2026-09-05 14:02:15 -0500

    Delete sw.js

--- sw.js
+++
@@ -1,4 +0,0 @@
-const CACHE='calendario21-v2';
-self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['/'])))});
-self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
-self.addEventListener('fetch',e=>{if(e.request.method==='GET'&&new URL(e.request.url).origin===location.origin)e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))});
